58383edad28ad3f9075f75527d808954d584aaab
Mikle <bastazf@mail.ru>
2019-12-17 05:42:44 +0300

    moved repeated code to func

--- hw6.py
+++ hw6.py
@@ -6,42 +6,29 @@ def bin_polinomic(dec_str):
     else:
         return False
 
+def sum_polynoms(poly_length):
+    if poly_length == 1:
+        return sum([x for x in range(1, 10) if bin_polinomic(str(x))])
+    insert_mid = poly_length % 2 == 1
+    pow = int((poly_length - 1) / 2) if insert_mid else int(poly_length / 2)
+    res = 0
+    if insert_mid:
+        for i in range(int(10**(pow-1)), int(10**pow)):
+            for j in range(10):
+                dec_str = str(i) + str(j) + str(i)[::-1]
+                if bin_polinomic(dec_str):
+                    res += int(dec_str)
+    else:
+        for i in range(int(10**(pow-1)), int(10**pow)):
+            dec_str = str(i) + str(i)[::-1]
+            if bin_polinomic(dec_str):
+                res += int(dec_str)
+    return res
+
 
 res = 0
-# полиномы длины 6
-for i in range(100, 1000):
-    dec_str = str(i) + str(i)[::-1]
-    if bin_polinomic(dec_str):
-        res += int(dec_str)
 
-# полиномы длины 4, 5
-for i in range(10, 100):
-    dec_str = str(i) + str(i)[::-1]
-    if bin_polinomic(dec_str):
-        res += int(dec_str) 
-
-    # вставляем в середину    
-    for j in range(10):
-        dec_str = str(i) + str(j) + str(i)[::-1]
-        if bin_polinomic(dec_str):
-            res += int(dec_str)
-
-# полиномы длины 2, 3
-for i in range(1, 10):
-    dec_str = str(i) + str(i)[::-1]
-    if bin_polinomic(dec_str):
-        res += int(dec_str)
-
-    # вставляем в середину    
-    for j in range(10):
-        dec_str = str(i) + str(j) + str(i)[::-1]
-        if bin_polinomic(dec_str):
-            res += int(dec_str)
-
-# полиномы длины 1
-for i in range(1, 10):
-    dec_str = str(i)
-    if bin_polinomic(dec_str):
-        res += int(dec_str)
-
+for i in range(1, 7):
+    res += sum_polynoms(i)
+    
 print(res)
